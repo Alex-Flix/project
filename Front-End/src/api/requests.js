88364935c8dbf7abab90b6a9bd7cbeb8f3bcac74
Adel-George <adel.george.isaac@gmail.com>
2023-07-05 02:37:23 +0300

    fix video issues

--- Front-End/src/api/requests.js
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "./config";
-
-export const searchForVid = (type, category, name) => {
-  return axiosInstance.get("movies/search", {
-    params: { type, category, name },
-  });
-};
